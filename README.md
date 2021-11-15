7c25844d6beb953a21c5a5872252df37d4e75236
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-11-15 21:49:11 -0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# microservices-spring-study
+ Small bank project to lean about microservices, spring boot, docker and kubernetes
---
+++ loans/src/test/java/com/ratz/loans/LoansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratz.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
