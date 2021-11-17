f0b71389af854c18c9a38616ea4d257cb78b08da
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-11-17 14:49:31 -0100

    created Properties entity and accounts service config in accounts microservice. connected accounts microservice to our config server. created endpoint to read and display properties that our configserver read from git repo

--- accounts/src/main/java/com/ratz/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/ratz/accounts/controller/AccountsController.java
@@ -1,12 +1,20 @@
 package com.ratz.accounts.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.ratz.accounts.config.AccountsServiceConfig;
 import com.ratz.accounts.entity.Accounts;
 import com.ratz.accounts.entity.Customer;
+import com.ratz.accounts.entity.Properties;
 import com.ratz.accounts.repository.AccountsRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class AccountsController {
@@ -14,14 +22,32 @@ public class AccountsController {
     @Autowired
     private AccountsRepository repository;
 
+    @Autowired
+    private AccountsServiceConfig accountsServiceConfig;
+
     @PostMapping("/myAccount")
     public Accounts getAccountDetail(@RequestBody Customer customer) {
         Accounts account = repository.findByCustomerId(customer.getCustomerId());
 
-        if(account != null) {
+        if (account != null) {
             return account;
         } else {
             return null;
         }
     }
+
+
+    @GetMapping("/account/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+
+        Properties properties = new Properties(accountsServiceConfig.getMsg(), accountsServiceConfig.getBuildVersion(),
+                accountsServiceConfig.getMailDetails(), accountsServiceConfig.getActiveBranches());
+
+        String jsonStr = ow.writeValueAsString(properties);
+
+        return jsonStr;
+    }
+
 }
