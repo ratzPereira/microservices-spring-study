ade47098b4a4c084b2cbb3f5f8e284c4bf8592a8
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-11-17 15:48:01 -0100

    Added loans and card config services and endpoints to get our data from repo. created 3 new docker compose files to start all microservices with one command

--- cards/src/main/java/com/ratz/cards/controller/CardsController.java
+++ cards/src/main/java/com/ratz/cards/controller/CardsController.java
@@ -1,9 +1,15 @@
 package com.ratz.cards.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.ratz.cards.config.CardsServiceConfig;
 import com.ratz.cards.entity.Cards;
 import com.ratz.cards.entity.Customer;
+import com.ratz.cards.entity.Properties;
 import com.ratz.cards.repository.CardsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,15 +22,27 @@ public class CardsController {
     @Autowired
     private CardsRepository repository;
 
+    @Autowired
+    private CardsServiceConfig cardsConfig;
+
     @PostMapping("myCards")
     public List<Cards> getCardsDetails(@RequestBody Customer customer) {
 
         List<Cards> cards = repository.findByCustomerId(customer.getCustomerId());
 
-        if(cards != null) {
+        if (cards != null) {
             return cards;
         } else {
             return null;
         }
+    }
+
+    @GetMapping("/cards/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(cardsConfig.getMsg(), cardsConfig.getBuildVersion(),
+                cardsConfig.getMailDetails(), cardsConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 }
--- loans/src/main/java/com/ratz/loans/controller/LoanController.java
+++ loans/src/main/java/com/ratz/loans/controller/LoanController.java
@@ -1,9 +1,15 @@
 package com.ratz.loans.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.ratz.loans.config.LoansServiceConfig;
 import com.ratz.loans.entity.Customer;
 import com.ratz.loans.entity.Loans;
+import com.ratz.loans.entity.Properties;
 import com.ratz.loans.repository.LoansRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,17 +22,29 @@ public class LoanController {
     @Autowired
     private LoansRepository repository;
 
+    @Autowired
+    private LoansServiceConfig loansConfig;
+
     @PostMapping("/myLoans")
-    public  List<Loans> getLoansDetails (@RequestBody Customer customer){
+    public List<Loans> getLoansDetails(@RequestBody Customer customer) {
 
         List<Loans> loans = repository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
 
-        if(loans != null) {
+        if (loans != null) {
             return loans;
         } else {
             return null;
         }
 
+    }
+
+    @GetMapping("/loans/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(loansConfig.getMsg(), loansConfig.getBuildVersion(),
+                loansConfig.getMailDetails(), loansConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 
 }
