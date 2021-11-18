4c9cc9d3d0bb2ec0821beaba3d7d18e3633a23a0
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-11-18 15:58:34 -0100

    added custom routing. now we can access from ratzbank/accounts and it will reedirect for /accounts.

--- gatewayserver/src/main/java/com/ratz/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/ratz/gatewayserver/GatewayserverApplication.java
@@ -2,7 +2,12 @@ package com.ratz.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +17,24 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/ratzbank/accounts/**")
+						.filters(f -> f.rewritePath("/ratzbank/accounts/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time",new Date().toString()))
+						.uri("lb://ACCOUNTS")).
+				route(p -> p
+						.path("/ratzbank/loans/**")
+						.filters(f -> f.rewritePath("/ratzbank/loans/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time",new Date().toString()))
+						.uri("lb://LOANS")).
+				route(p -> p
+						.path("/ratzbank/cards/**")
+						.filters(f -> f.rewritePath("/ratzbank/cards/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time",new Date().toString()))
+						.uri("lb://CARDS")).build();
+	}
 }
